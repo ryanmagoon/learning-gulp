0aaaa4d7cb44b2cd2af037abab2fd8b40c38581f
Ryan Magoon <mr.magoon5@gmail.com>
2016-08-15 23:23:31 -0400

    added sass compilation and sourcemaps support

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // grab our gulp packages
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
@@ -12,7 +14,15 @@ gulp.task('jshint', function() {
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('build-css', function() {
+    return gulp.src('source/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/assets/stylesheets'));
+});
+
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
     gulp.watch('source/javascript/**/*.js', ['jshint']);
+    gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
